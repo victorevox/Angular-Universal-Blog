e563dcff420d8510fec487d5c7983d97750bfc2f
Victor HG <victorevox@gmail.com>
2018-11-10 14:07:29 -0600

    Upgrade Angular7

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
-import { NgModule, Injector } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from "@angular/forms";
-import { HttpModule, XHRBackend, RequestOptions } from "@angular/http";
 import { HttpClientModule } from '@angular/common/http';
-// import { JasperoAlertsModule, AlertsService } from "@jaspero/ng-alerts";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FileUploadModule } from 'ng2-file-upload';
 import { SimpleNotificationsModule } from 'angular2-notifications';
@@ -15,9 +13,6 @@ import { BASE_PAGES } from "./pages";
 import { ROUTES } from "./app.routing";
 import { BASE_SERVICES } from "./services";
 import { GUARDS } from "./guards";
-
-import { HttpClient, HttpResponse } from "@angular/common/http";
-import { HttpInterceptors } from '@app/http-interceptors';
 
 @NgModule({
   declarations: [
@@ -30,7 +25,6 @@ import { HttpInterceptors } from '@app/http-interceptors';
     FormsModule,
     BrowserAnimationsModule,
     FileUploadModule,
-    // JasperoAlertsModule.forRoot(),
     SimpleNotificationsModule.forRoot(),
     BrowserModule.withServerTransition({appId: 'my-app'}),
     RouterModule.forRoot(ROUTES)
@@ -39,16 +33,7 @@ import { HttpInterceptors } from '@app/http-interceptors';
     // HttpInterceptors,
     ...BASE_SERVICES,
     ...GUARDS,
-    // {
-    //   provide: CustomHttpService,
-    //   useFactory: customHttp,
-    //   deps: [XHRBackend, RequestOptions, Injector /* AuthService */],
-    // }
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-// export function customHttp(backend: XHRBackend, options: RequestOptions, injector: Injector/* authService: AuthService */) {
-//   return new CustomHttpService(backend, options, injector/* authService */);
-// }
